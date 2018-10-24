3a1d6877bbddca054247fd2cdd73cce631f33290
Yuping Lu <yupinglu89@gmail.com>
2018-10-24 17:11:40 -0400

    update n0h

--- n0h3.py
+++ n0h3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# Get the most frequent radar_echo_classification without '10'
+# Get the most frequent radar_echo_classification without 0, 10, 20, 140, 150 and mode size > 20 
 # Author: Yuping Lu <yupinglu89@gmail.com>
-# Date: 10/23/2018
+# Date: 10/24/2018
 
 # load libs
 import pyart
@@ -24,7 +24,7 @@ cnt = {
     150 : 0 # Range Folded (RH)
 }
 
-fw= open("label_1.txt","w")
+fw= open("label_3.txt","w")
 
 with open('n0h.txt', 'r') as f:
     #counter = 0
@@ -34,15 +34,24 @@ with open('n0h.txt', 'r') as f:
         
         N0H = pyart.io.read('final/' + line.strip('\n'))
         x = N0H.fields['radar_echo_classification']['data']
-        mx = ma.masked_values(x, 10.0) #remove Biological (BI)
+        mx = ma.masked_values(x, 0.0) 
+        mx = ma.masked_values(mx, 10.0) 
+        mx = ma.masked_values(mx, 20.0)
+        mx = ma.masked_values(mx, 140.0) 
+        mx = ma.masked_values(mx, 150.0) 
         data = mx.compressed()
         if len(data) != 0:
-            res = mode(data)[0][0]
-            cnt[res] += 1
-            fw.write(str(int(res))+'\n')
-            #print(res)
+            d = mode(data)
+            if d[1][0] > 20:
+                res = d[0][0]
+                cnt[res] += 1
+                fw.write(str(int(res))+'\n')
+                #print(res)
+            else:
+                fw.write('-1\n')
         else:
             fw.write('-1\n')
+        
         #counter += 1
         
 fw.close()
