b50a42a3d5795a87bdb464289d6ea0d6be943a35
Yuping Lu <yupinglu89@gmail.com>
2018-11-21 13:56:53 -0500

    update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Last Update: 11/19/2018
 import os
 import argparse
 import random
-import time
 
 import torch
 import torch.nn as nn
@@ -19,12 +18,32 @@ from datasets.nexraddataset import *
 import models
 
 def train(args, model, device, train_loader, optimizer, criterion, epoch):
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    
     model.train()
+    train_loss = 0
+    correct = 0
+    acc = 0
+    
     for batch_idx, data in enumerate(train_loader):
         inputs, labels = data['radar'].to(device), data['category'].to(device)
-        optimizer.zero_grad()
+        
+        # compute output
         outputs = model(inputs)
         loss = criterion(outputs, labels)
+        
+        # measure accuracy and record loss
+        acc1 = accuracy(outputs, labels, topk=(1,))[0]
+        losses.update(loss.item(), inputs.size(0))
+        top1.update(acc1[0], inputs.size(0))
+        
+        train_loss += loss.item() # sum up batch loss
+        pred = outputs.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(labels).sum().item()
+        
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
@@ -32,25 +51,51 @@ def train(args, model, device, train_loader, optimizer, criterion, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, (batch_idx + 1) * len(inputs), len(train_loader.dataset),
                 100. * (batch_idx + 1) / len(train_loader), loss.item()))
-        
 
+    # print average loss and accuracy
+    train_loss /= len(train_loader)
+    acc = 100. * correct / len(train_loader.dataset)
+    print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        train_loss, correct, len(train_loader.dataset), acc))
+    
+    print(' * Loss@1 {loss.avg:.3f}'.format(loss=losses))
+    print(' * Acc@1 {top1.avg:.3f}'.format(top1=top1))
+    
 def test(args, model, device, test_loader, criterion):
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    
     model.eval()
     test_loss = 0
     correct = 0
     acc = 0
+    
     with torch.no_grad():
         for data in test_loader:
             inputs, labels = data['radar'].to(device), data['category'].to(device)
+            
+            # compute output
             outputs = model(inputs)
-            test_loss += criterion(outputs, labels).item() # sum up batch loss
+            loss = criterion(outputs, labels)
+            
+            # measure accuracy and record loss
+            acc1 = accuracy(outputs, labels, topk=(1,))[0]
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(acc1[0], inputs.size(0))
+            
+            test_loss += loss.item() # sum up batch loss
             pred = outputs.max(1)[1] # get the index of the max log-probability
             correct += pred.eq(labels).sum().item()
-            
+    
+    # print average loss and accuracy
     test_loss /= len(test_loader)
     acc = 100. * correct / len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset), acc))
+    
+    print(' * Loss@1 {loss.avg:.3f}'.format(loss=losses))
+    print(' * Acc@1 {top1.avg:.3f}'.format(top1=top1))
+    
     return acc
 
 def main():
@@ -171,6 +216,39 @@ def adjust_learning_rate(optimizer, epoch, args):
     lr = args.lr * (0.1 ** ((epoch-1) // 150))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
             
 if __name__ == '__main__':
     main()
